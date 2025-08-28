d1d5d49361ac666c4993279852bfba01b998126f
Pacifique SUGIRA <114115881+Paccy-SUGIRA@users.noreply.github.com>
2025-08-28 17:00:13 +0200

    Update wsgi.py

--- GCP/wsgi.py
+++ GCP/wsgi.py
@@ -4,6 +4,6 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "GCP.settings")
 application = get_wsgi_application()
 
-# Start scheduler after Django is configured
+# Import and start scheduler after Django is initialized
 from gwizacash.scheduler import start_scheduler
 start_scheduler()
