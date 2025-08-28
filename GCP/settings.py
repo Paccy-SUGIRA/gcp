41c778548c5c0fcc186cd6254f0759f6ced6ce15
Pacifique SUGIRA <114115881+Paccy-SUGIRA@users.noreply.github.com>
2025-08-28 16:21:09 +0200

    Update settings.py

--- GCP/settings.py
+++ GCP/settings.py
@@ -94,7 +94,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR)
 
 # Media files
 MEDIA_URL = '/media/'
